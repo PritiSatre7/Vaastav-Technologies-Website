e007cd94b7ba125b909deb3270b14a856fafdaeb
PritiSatre7 <priti.satre@vaastav.tech>
2026-03-21 14:17:23 +0530

    feat: update splash screen

---
+++ frontend/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setTimeout(() => {
+        document.getElementById("splash").style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+    }, 1000);
+};
